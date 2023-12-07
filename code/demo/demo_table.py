094770060aaf879370995221605d2422d36a9baf
Lydia Liu <yuzhu.liu@utexas.edu>
2023-12-07 02:01:49 -0600

    finalize table creation

--- code/demo/demo_table.py
+++ code/demo/demo_table.py
@@ -2,14 +2,15 @@ import pandas as pd
 from database import engine
 from sqlalchemy import text
 
-demo_data = pd.read_csv("demo/demographic_data_no_header.csv", header=None)
-
-print(demo_data.head())
+demo_data = pd.read_csv("data/demographic_data/demographic_data.csv", header=None, skiprows=1)
+demo_data.to_csv("data/demographic_data/demographic_data_no_header.csv", index=False, header=False)
+# print(demo_data.head())
 
 create_extension = text("CREATE EXTENSION IF NOT EXISTS postgis")
 
 with engine.connect() as connection:
     connection.execute(create_extension)
+
 
 create_table_sql = text("""
     CREATE TABLE demographics (
